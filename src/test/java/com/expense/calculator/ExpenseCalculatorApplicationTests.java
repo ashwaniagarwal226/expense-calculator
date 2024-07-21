fe71e815fba30451e89d48bfb89dd417a8653270
ashwiniagarwal <ashwaniagarwal226@gmail.com>
2024-07-21 22:13:12 +0530

    expense tracker intial commit

---
+++ src/test/java/com/expense/calculator/ExpenseCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
